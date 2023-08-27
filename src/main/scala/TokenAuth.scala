0dff8620103c2a1202257c33bd8163309628f84c
Larry Finkelstein <larryf1@gmail.com>
2023-08-26 20:52:05 -0400

    Add JWT token support

---
+++ src/main/scala/TokenAuth.scala
@@ -0,0 +1,75 @@
+import cats.effect.{ExitCode, IO, IOApp, Resource}
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.ember.server._
+import com.comcast.ip4s._
+import cats.implicits._
+import dev.profunktor.auth._
+import dev.profunktor.auth.jwt._
+import pdi.jwt._
+
+import java.time.Instant
+import io.circe._
+import io.circe.parser._
+import org.http4s.server.Server
+
+object TokenAuth extends IOApp {
+  case class AuthUser(id: Long, name: String)
+  case class TokenPayLoad(user: String, level: String)
+
+  object TokenPayLoad {
+    implicit def decoder: Decoder[TokenPayLoad] = Decoder.instance { h =>
+      for {
+        user <- h.get[String]("user")
+        level <- h.get[String]("level")
+      } yield TokenPayLoad(user, level)
+    }
+  }
+
+  private val claim = JwtClaim(content = """{"user":"John", "level":"basic"}""",
+    expiration = Some(Instant.now.plusSeconds(60*15).getEpochSecond),
+    issuedAt = Some(Instant.now.getEpochSecond))
+
+  private val key = "secretKey"
+
+  private val algo = JwtAlgorithm.HS256
+
+  private val token = JwtCirce.encode(claim, key, algo)
+
+  private val database = Map("John" -> AuthUser(123, "JohnDoe"))
+
+  private val authenticate: JwtToken => JwtClaim => IO[Option[AuthUser]] = (token: JwtToken) => (claim: JwtClaim) => {
+    decode[TokenPayLoad](claim.content) match {
+      case Right(payload) => IO(database.get(payload.user))
+      case Left(_) => IO(None)
+    }
+  }
+
+  private val jwtAuth = JwtAuth.hmac(key, algo)
+  private val middleware = JwtAuthMiddleware[IO, AuthUser](jwtAuth, authenticate)
+
+  val authedRoutes: AuthedRoutes[AuthUser, IO] =
+    AuthedRoutes.of {
+      case GET -> Root / "welcome" as user =>
+        Ok(s"Welcome, ${user.name}")
+    }
+
+  private val loginRoutes: HttpRoutes[IO] =
+    HttpRoutes.of[IO] {
+      case GET -> Root / "login" =>
+        Ok(s"Logged In").map(_.addCookie(ResponseCookie("token", token)))
+    }
+
+  private val securedRoutes: HttpRoutes[IO] = middleware(authedRoutes)
+
+  private val service = loginRoutes <+> securedRoutes
+
+  val server: Resource[IO, Server] = EmberServerBuilder
+    .default[IO]
+    .withHost(ipv4"0.0.0.0")
+    .withPort(port"8080")
+    .withHttpApp(service.orNotFound)
+    .build
+
+  override def run(args: List[String]): IO[ExitCode] = server.use(_ => IO.never).as(ExitCode.Success)
+}
